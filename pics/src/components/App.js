c7a7b6701e888d58bd4328145e9c0596a43b57cd
Taesun Lee <superbsun@gmail.com>
2021-01-24 23:47:14 +0900

    handling user input with forms and events
    - using callback func from child to parents

---
+++ pics/src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+
+class App extends React.Component {
+    onSearchSubmit(term) {}
+
+    render() {
+        <div className="ui container" style={{ marginTop: "10px" }}>
+            <SearchBar onSubmit={this.onSearchSubmit} />
+        </div>;
+    }
+}
+
+export default App;
---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+    state = { term: "Hi there!!!" };
+
+    // onFormSubmit(event) { } -> this is undefined inside the left style function
+    onFormSubmit = (event) => {
+        event.preventDefault(); // it prevents to refresh page
+
+        this.props.onSubmit(event.state.term);
+    };
+
+    render() {
+        return (
+            <div className="ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={(e) =>
+                                this.setState({
+                                    term: e.target.value.toUpperCase,
+                                })
+                            }
+                        ></input>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
